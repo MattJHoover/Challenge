b1204eb45158032edf1f3a7fbebdf27558205d69
Matthew Hoover <mhoover123@gmail.com>
2018-06-11 21:09:18 -0400

    Update ReportingStructureController.java

--- senior-code-challenge/src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
+++ senior-code-challenge/src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
@@ -1,7 +1,7 @@
 package com.mindex.challenge.controller;
 
-import com.mindex.challenge.data.Employee;
-import com.mindex.challenge.service.EmployeeService;
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.ReportingStructureService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,25 +14,10 @@ public class ReportingStructureController {
     @Autowired
     private ReportingStructureService reportingStructureService;
 
-    @PostMapping("/employee")
-    public Employee create(@RequestBody Employee employee) {
-        LOG.debug("Received employee create request for [{}]", employee);
-
-        return employeeService.create(employee);
-    }
-
     @GetMapping("/reportingStructure/{id}")
     public ReportingStructure read(@PathVariable String id) {
         LOG.debug("Received reporting structure create request for id [{}]", id);
 
         return reportingStructureService.read(id);
-    }
-
-    @PutMapping("/reportingStructure/{id}")
-    public ReportingStructure update(@PathVariable String id, @RequestBody Employee employee) {
-        LOG.debug("Received employee create request for id [{}] and employee [{}]", id, employee);
-
-        employee.setEmployeeId(id);
-        return employeeService.update(employee);
     }
 }
