9b170f7884904f3bea9c2130add0e5133f6a2521
Matthew Hoover <mhoover123@gmail.com>
2018-06-12 02:27:49 -0400

    Added task 2

---
+++ senior-code-challenge/src/main/java/com/mindex/challenge/data/Compensation.java
@@ -0,0 +1,25 @@
+package com.mindex.challenge.data;
+
+public class Compensation {
+	private Employee employee;
+	private Double salary;
+	
+	public Compensation() {
+	}
+	
+	public Employee getEmployee() {
+		return employee;
+	}
+	
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	
+	public Double getSalary() {
+		return salary;
+	}
+	
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+}
